400a5aaba1d46d8adca654fc8743ca0c105ae22b
The-Estonian <Zaar2213@gmail.com>
2023-11-27 18:08:18 +0200

    started min max

---
+++ min-max.js
@@ -0,0 +1,13 @@
+const max = (var1, var2) => {
+  if (var1 > var2) {
+    return var1;
+  }
+  return var2;
+};
+
+const min = (var1, var2) => {
+  if (var1 < var2) {
+    return var1;
+  }
+  return var2;
+};
