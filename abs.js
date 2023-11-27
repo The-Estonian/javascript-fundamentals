8a28a8b6a9b0c0f1c3e830301a1c5581172d09fa
The-Estonian <Zaar2213@gmail.com>
2023-11-27 18:03:50 +0200

    fixed another edge case

--- abs.js
+++ abs.js
@@ -1,11 +1,14 @@
 const isPositive = (n) => {
-  if (n >= 0) {
+  if (n > 0) {
     return true;
   }
   return false;
 };
 
 const abs = (n) => {
+  if (n === 0) {
+    return n;
+  }
   if (isPositive(n)) {
     return n;
   }
