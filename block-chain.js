399d86b004e571b89b8aebc7e0e19c9cbcf9a118
The-Estonian <Zaar2213@gmail.com>
2023-11-27 21:05:18 +0200

    work save on block chain proj

---
+++ block-chain.js
@@ -0,0 +1,19 @@
+const blockChain = (data, prev) => {
+  if (prev === null) {
+    prev = { index: 0, hash: '0' };
+  }
+  return {
+    index:
+    hash: hashCode()
+    data:
+    prev:
+    chain:
+  }
+};
+
+// must return new object with those stats or null version
+// index ?
+// hash: has is func prov by them, needs index.string() prev.hash? and data stringify?
+// data
+// prev
+// chain
