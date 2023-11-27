852a31a6fa35c1bf9e4d5819106a7901253a7d6e
The-Estonian <Zaar2213@gmail.com>
2023-11-27 18:43:37 +0200

    started is

---
+++ is.js
@@ -0,0 +1,43 @@
+is.num = (n) => {
+  return typeof n === 'number' && isFinite(n);
+};
+
+is.nan = (n) => {
+  return isNan(n);
+};
+
+is.str = (n) => {
+  return typeof n === 'string' || n instanceof String;
+};
+
+is.bool = (n) => {
+  return typeof n === 'boolean';
+};
+
+is.undef = (n) => {
+  return n === undefined;
+};
+
+is.def = (n) => {
+  return !is.undefined(n);
+};
+
+is.arr = (arr) => {
+  return Array.isArray(arr);
+};
+
+is.obj = (obj) => {
+  return typeof n === 'object';
+};
+
+is.fun = (fun) => {
+  return typeof n === 'function';
+};
+
+is.truthy = (item) => {
+  return !!item;
+};
+
+is.falsy = (item) => {
+  return !item;
+};
