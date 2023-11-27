38699aa1274bd0080903196b087d599f9ddf5030
The-Estonian <Zaar2213@gmail.com>
2023-11-27 15:42:42 +0200

    fixed logic mistake

--- dog-years.js
+++ dog-years.js
@@ -7,30 +7,20 @@ const dogYears = (planet, ageSeconds) => {
     case 'earth':
       return Math.round(baseCalc(ageSeconds, 7) * 100) / 100;
     case 'mercury':
-      return (
-        Math.round(baseCalc(ageSeconds, 7 * 0.2408467) * 10000000) / 10000000
-      );
+      return Math.round(baseCalc(ageSeconds, 7 / 0.2408467) * 100) / 100;
     case 'venus':
-      return (
-        Math.round(baseCalc(ageSeconds, 7 * 0.61519726) * 100000000) / 100000000
-      );
+      return Math.round(baseCalc(ageSeconds, 7 / 0.61519726) * 100) / 100;
     case 'mars':
-      return (
-        Math.round(baseCalc(ageSeconds, 7 * 1.8808158) * 10000000) / 10000000
-      );
+      return Math.round(baseCalc(ageSeconds, 7 / 1.8808158) * 100) / 100;
     case 'jupiter':
-      return (
-        Math.round(baseCalc(ageSeconds, 7 * 11.862615) * 1000000) / 1000000
-      );
+      return Math.round(baseCalc(ageSeconds, 7 / 11.862615) * 100) / 100;
     case 'saturn':
-      return (
-        Math.round(baseCalc(ageSeconds, 7 * 29.447498) * 1000000) / 1000000
-      );
+      return Math.round(baseCalc(ageSeconds, 7 / 29.447498) * 100) / 100;
     case 'uranus':
-      return (
-        Math.round(baseCalc(ageSeconds, 7 * 84.016846) * 1000000) / 1000000
-      );
+      return Math.round(baseCalc(ageSeconds, 7 / 84.016846) * 100) / 100;
     case 'neptune':
-      return Math.round(baseCalc(ageSeconds, 7 * 164.79132) * 100000) / 100000;
+      return Math.round(baseCalc(ageSeconds, 7 / 164.79132) * 100) / 100;
   }
 };
+
+console.log(dogYears('mercury', 2134835688));
