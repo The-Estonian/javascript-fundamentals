427057421c21d19183a8ad2d0c6cb23338bb3245
The-Estonian <Zaar2213@gmail.com>
2023-11-27 15:40:05 +0200

    added rounding to calculations

--- dog-years.js
+++ dog-years.js
@@ -5,25 +5,32 @@ const baseCalc = (seconds, multiplier) => {
 const dogYears = (planet, ageSeconds) => {
   switch (planet) {
     case 'earth':
-      return baseCalc(ageSeconds, 7);
+      return Math.round(baseCalc(ageSeconds, 7) * 100) / 100;
     case 'mercury':
-      return baseCalc(ageSeconds, 7 * 0.2408467);
+      return (
+        Math.round(baseCalc(ageSeconds, 7 * 0.2408467) * 10000000) / 10000000
+      );
     case 'venus':
-      return baseCalc(ageSeconds, 7 * 0.61519726);
-
+      return (
+        Math.round(baseCalc(ageSeconds, 7 * 0.61519726) * 100000000) / 100000000
+      );
     case 'mars':
-      return baseCalc(ageSeconds, 7 * 1.8808158);
-
+      return (
+        Math.round(baseCalc(ageSeconds, 7 * 1.8808158) * 10000000) / 10000000
+      );
     case 'jupiter':
-      return baseCalc(ageSeconds, 7 * 11.862615);
-
+      return (
+        Math.round(baseCalc(ageSeconds, 7 * 11.862615) * 1000000) / 1000000
+      );
     case 'saturn':
-      return baseCalc(ageSeconds, 7 * 29.447498);
-
+      return (
+        Math.round(baseCalc(ageSeconds, 7 * 29.447498) * 1000000) / 1000000
+      );
     case 'uranus':
-      return baseCalc(ageSeconds, 7 * 84.016846);
-
+      return (
+        Math.round(baseCalc(ageSeconds, 7 * 84.016846) * 1000000) / 1000000
+      );
     case 'neptune':
-      return baseCalc(ageSeconds, 7 * 164.79132);
+      return Math.round(baseCalc(ageSeconds, 7 * 164.79132) * 100000) / 100000;
   }
 };
