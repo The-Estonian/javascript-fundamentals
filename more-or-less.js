04af80d05630eea62ed8a3cdcad33050d8bc6caa
The-Estonian <Zaar2213@gmail.com>
2023-11-27 11:35:47 +0200

    more or less started

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+const more = (e) => {
+  return e++;
+};
+
+const less = (e) => {
+  return e--;
+};
+
+const add = (var1, var2) => {
+  return var1 + var2;
+};
+
+const sub = (var1, var2) => {
+  return var1 - var2;
+};
