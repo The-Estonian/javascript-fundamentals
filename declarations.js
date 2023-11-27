dc32f0174a7e2c92057ff88b2694b7d4e1830a71
The-Estonian <Zaar2213@gmail.com>
2023-11-27 10:39:44 +0200

    finished primitives and started declarations

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = `\`\\/"'`;
+const arr = [4, '2'];
+const obj = {
+  str: 'a',
+  num: 1,
+  bool: true,
+  undef: undefined,
+};
+const nested = {
+  arr: [4, undefined, '2'],
+  obj: {
+    str: 'b',
+    num: 2,
+    bool: false,
+  },
+};
