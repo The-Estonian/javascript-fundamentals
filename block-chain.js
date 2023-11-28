d130cd532b73c5c62ba9b81134086c3f70088980
The-Estonian <Zaar2213@gmail.com>
2023-11-28 14:43:42 +0200

    block chain try

--- block-chain.js
+++ block-chain.js
@@ -1,19 +1,29 @@
 const blockChain = (data, prev) => {
-  if (prev === null) {
+  if (prev == null) {
     prev = { index: 0, hash: '0' };
   }
   return {
-    index:
-    hash: hashCode()
-    data:
-    prev:
-    chain:
-  }
+    index: prev.index + 1,
+    hash: hashCode(String(prev.index + 1) + prev.hash + JSON.stringify(data)),
+    data: data,
+    prev: prev,
+    chain: (data) => {
+      return blockChain(data, this);
+    },
+  };
 };
 
 // must return new object with those stats or null version
-// index ?
-// hash: has is func prov by them, needs index.string() prev.hash? and data stringify?
-// data
-// prev
-// chain
+// index provided by script, needs to be incremented after each block
+// hash: hashCOde is func provided by script, needs index.string() prev.hash? and data stringify? To generate unique hash key for the block that has info about previous and next block
+// data: input data
+// prev: previous node
+// chain: func for recursion for Next(data)
+
+// const hashCode = (str) =>
+//   (
+//     [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
+//   ).toString(36);
+
+// let first = blockChain({ a: 1 });
+// console.log(first.hash);
