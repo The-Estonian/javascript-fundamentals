61dbc11f55ebc8aa5434c35a2b7338df51f71bca
The-Estonian <Zaar2213@gmail.com>
2023-11-28 15:20:31 +0200

    function fix

--- block-chain.js
+++ block-chain.js
@@ -4,10 +4,12 @@ const blockChain = (data, prev) => {
   }
   return {
     index: prev.index + 1,
-    hash: hashCode((prev.index + 1).toString() + prev.hash + JSON.stringify(data)),
+    hash: hashCode(
+      (prev.index + 1).toString() + prev.hash + JSON.stringify(data)
+    ),
     data: data,
     prev: prev,
-    chain: (data) => {
+    chain: function (data) {
       return blockChain(data, this);
     },
   };
@@ -27,3 +29,29 @@ const blockChain = (data, prev) => {
 
 // let first = blockChain({ a: 1 });
 // console.log(first.hash);
+
+// const first = blockChain({ a: 1 });
+// console.log(first.index); //           -> 1
+// console.log(first.data); //            -> { a: 1 }
+// console.log(first.prev); //            -> { index: 0, hash: "0" }
+// console.log(first.hash); //            -> '1103f27'
+// console.log(hashCode('10{"a":1}')); // -> '1103f27'
+
+// const second = first.chain({ hello: 'world' });
+// console.log(second.hash); //                           -> '18drvvc'
+// console.log(hashCode('21103f27{"hello":"world"}')); // -> '18drvvc'
+
+// const chain = second
+//   .chain({ value: 4455 })
+//   .chain({ some: 'data' })
+//   .chain({ cool: 'stuff' });
+
+// const fork = second
+//   .chain({ value: 335 })
+//   .chain({ some: 'data' })
+//   .chain({ cool: 'stuff' });
+
+// console.log(chain.hash); //  -> '1qr3qfs'
+// console.log(fork.hash); //   -> '1x9gsc1'
+// console.log(chain.index); // -> 5
+// console.log(fork.index); //  -> 5
