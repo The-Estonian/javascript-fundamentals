a6f69e34d0f76832c1dd8c7eb374bcdd27a08271
The-Estonian <Zaar2213@gmail.com>
2023-11-29 15:28:24 +0200

    fixed edge case of undefined

--- get.js
+++ get.js
@@ -1,12 +1,16 @@
 const get = (src, path) => {
   let loops = path.split('.');
   let returnObj = src;
-  for (let index = 0; index < loops.length; index++) {
-    returnObj = returnObj[loops[index]];
+  try {
+    for (let index = 0; index < loops.length; index++) {
+      returnObj = returnObj[loops[index]];
+    }
+    return returnObj;
+  } catch {
+    return undefined;
   }
-  return returnObj;
 };
 
-// const src = { nested: { key: 'peekaboo' } };
-// const path = 'nested.key';
-// console.log(get(src, path));
+const src = { nested: { key: 'peekaboo' } };
+const path = 'nx.nx';
+console.log(get(src, path));
