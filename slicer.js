8a800f1c100a1c21e47c4bcca1f53d931abef31f
The-Estonian <Zaar2213@gmail.com>
2023-11-29 10:56:56 +0200

    added array/string functionality

--- slicer.js
+++ slicer.js
@@ -1,10 +1,17 @@
 const slice = (arr, startIndx, endIndx = arr.length) => {
-  var returnSlice = [];
+  if (Array.isArray(arr)) {
+    var returnSlice = [];
+    for (let index = startIndx; index < endIndx; index++) {
+      returnSlice.push(arr[index]);
+    }
+    return returnSlice;
+  }
+  var returnSlice = '';
   for (let index = startIndx; index < endIndx; index++) {
-    returnSlice.push(arr[index])
+    returnSlice += arr[index];
   }
   return returnSlice;
 };
 
-
 console.log(slice([1, 2, 3, 4, 5], 2));
+console.log(slice('abcdef', 2));
