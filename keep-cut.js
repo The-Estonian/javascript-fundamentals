20724342c09df749be0994454469fc26cbf46980
The-Estonian <Zaar2213@gmail.com>
2023-11-29 11:15:40 +0200

    started keep-cut

---
+++ keep-cut.js
@@ -0,0 +1,30 @@
+const cutFirst = (str) => {
+  return str.slice(2);
+};
+
+const cutLast = (str) => {
+  return str.slice(0, -2);
+};
+
+const cutFirstLast = (str) => {
+  return str.slice(2, -2);
+};
+
+const keepFirst = (str) => {
+  return str.slice(0, 2);
+};
+
+const keepLast = (str) => {
+  return str.slice(-2);
+};
+
+const keepFirstLast = (str) => {
+  return keepFirst(str) + keepLast(str);
+};
+
+console.log(cutFirst('abcdefghij'));
+console.log(cutLast('abcdefghij'));
+console.log(cutFirstLast('abcdefghij'));
+console.log(keepFirst('abcdefghij'));
+console.log(keepLast('abcdefghij'));
+console.log(keepFirstLast('abcdefghij'));
