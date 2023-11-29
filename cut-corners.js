6b42180cd95818c602eb0484af36db38edeb8b4c
The-Estonian <Zaar2213@gmail.com>
2023-11-29 12:34:10 +0200

    trying hexadecimals in code

--- cut-corners.js
+++ cut-corners.js
@@ -68,6 +68,11 @@ const floor = (n) => {
 };
 
 const trunc = (n) => {
+  var ff = false;
+  if (n > 0xfffffffff) {
+    n -= 0xfffffffff;
+    ff = true;
+  }
   var trigger = false;
   if (n < 0) {
     trigger = true;
@@ -85,16 +90,22 @@ const trunc = (n) => {
     counter++;
     n--;
   }
+  if (ff) {
+    return 0xfffffffff + counter;
+  }
   return counter;
 };
 
-// const nums = [3.7, -3.7, 3.1, -3.1];
-// console.log(nums.map(round));
-// console.log(nums.map(floor));
-// console.log(nums.map(trunc));
-// console.log(nums.map(ceil));
+const nums = [3.7, -3.7, 3.1, -3.1];
+console.log(nums.map(round));
+console.log(nums.map(floor));
+console.log(nums.map(trunc));
+console.log(nums.map(ceil));
 
 // [4, -4, 3, -3]
 // [3, -4, 3, -4]
 // [3, -3, 3, -3]
 // [4, -3, 4, -3]
+
+// https://01.kood.tech/git/root/public/src/commit/7913d11bec0c573d8690e029734fa3489f9a81f8/js/tests/cut-corners_test.js
+// right...
