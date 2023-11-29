4dccc75fb46d9084812aca602fac45d3746ddbeb
The-Estonian <Zaar2213@gmail.com>
2023-11-29 16:09:55 +0200

    fixed multicharacter icons

--- pyramid.js
+++ pyramid.js
@@ -1,12 +1,20 @@
 const pyramid = (str, num) => {
   let returnString = '';
+  // num amount of rows
   for (let i = 0; i < num; i++) {
+    // fill spaces before str icons
     for (let j = 0; j < num - i - 1; j++) {
-      returnString += ' ';
+      let filler = '';
+      for (let y = 0; y < str.length; y++) {
+        filler += ' ';
+      }
+      returnString += filler;
     }
+    // fill str icons
     for (let x = 0; x <= i * 2; x++) {
       returnString += str;
     }
+    // new line after each iteration
     if (i != num - 1) {
       returnString += '\n';
     }
@@ -14,4 +22,4 @@ const pyramid = (str, num) => {
   return returnString;
 };
 
-// console.log(pyramid('*', 20));
+console.log(pyramid('{}', 12));
