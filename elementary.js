fdda15a7db43fe178dab974cd40720019921336d
The-Estonian <Zaar2213@gmail.com>
2023-11-29 09:56:46 +0200

    fixed logic mistake on multiply

--- elementary.js
+++ elementary.js
@@ -1,13 +1,25 @@
 const multiply = (a, b) => {
-  var returnSum = 0
+  var trigger = false;
+  if (a < 0) {
+    a = 0 - a;
+    trigger = !trigger;
+  }
+  if (b < 0) {
+    b = 0 - b;
+    trigger = !trigger;
+  }
+  var returnSum = 0;
   for (let index = 0; index < b; index++) {
     returnSum += a;
+  }
+  if (trigger) {
+    returnSum = returnSum - returnSum - returnSum;
   }
   return returnSum;
 };
 
 const divide = (a, b) => {
-  var returnSum = a
+  var returnSum = a;
   for (let index = 0; index < a; index++) {
     if (returnSum >= b) {
       returnSum -= b;
@@ -27,5 +39,3 @@ const modulo = (a, b) => {
     }
   }
 };
-
-console.log(modulo(100, 6));
