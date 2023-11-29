450978a2c38b068e2fb756743ed4fd15ed831c64
The-Estonian <Zaar2213@gmail.com>
2023-11-29 23:29:58 +0200

    new flat

--- flat.js
+++ flat.js
@@ -1,20 +1,15 @@
-// const flat = (arr, depth) => {
-//   let newArray = [];
-//   if (depth == 1) {
-//     return newArray;
-//   }
-//   for (let i = 0; i < arr.length; i++) {
-//     if (Array.isArray(arr[i])) {
-//       newArray = newArray.concat(flat(arr[i]), depth - 1);
-//     } else {
-//       newArray.push(arr[i]);
-//     }
-//   }
-//   return newArray;
-// };
+const flat = (arr, depth = 1) => {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) && depth > 0) {
+      newArray = newArray.concat(flat(arr[i], depth - 1));
+    } else {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+};
 
-// let items = [0, 1, [2, [3, [4, 5]]]];
+let items = [0, 1, [2, [3, [4, 5]]]];
 
-// console.log(flat([0, 1, [2, [3, [4, 5]]]]));
-let trigger = '';
-console.log('hello'.slice(0, 1+trigger.length) === trigger);
+// console.log(flat([0, 1, [2, [3, [4, 5]]]], Infinity));
