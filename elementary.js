e126dd58375b9aca19b014286015225c90fef3ac
The-Estonian <Zaar2213@gmail.com>
2023-11-29 09:47:18 +0200

    Read the actual requirements and implemented them

--- elementary.js
+++ elementary.js
@@ -1,11 +1,31 @@
 const multiply = (a, b) => {
-  return a * b;
+  var returnSum = 0
+  for (let index = 0; index < b; index++) {
+    returnSum += a;
+  }
+  return returnSum;
 };
 
 const divide = (a, b) => {
-  return a / b;
+  var returnSum = a
+  for (let index = 0; index < a; index++) {
+    if (returnSum >= b) {
+      returnSum -= b;
+    } else {
+      return index;
+    }
+  }
 };
 
 const modulo = (a, b) => {
-  return a % b;
+  var returnSum = a;
+  for (let index = 0; index < a; index++) {
+    if (returnSum >= b) {
+      returnSum -= b;
+    } else {
+      return returnSum;
+    }
+  }
 };
+
+console.log(modulo(100, 6));
