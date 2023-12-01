2bbd058c8609930a33e597672b28c14fe4c204bc
The-Estonian <Zaar2213@gmail.com>
2023-12-01 12:17:47 +0200

    should compare amount of matches not the length of the match

--- same-amount.js
+++ same-amount.js
@@ -1,14 +1,11 @@
 const sameAmount = (str, exp1, exp2) => {
-  let testCounter = 0;
   console.log(str);
   let regex1 = str.match(exp1);
   let regex2 = str.match(exp2);
-  //   console.log(regex1[0].length);
-  //   console.log(regex2[0].length);
+  // console.log(regex1.length);
+  // console.log(regex2.length);
   if (regex1 !== null && regex2 !== null) {
-    console.log(testCounter);
-    testCounter++;
-    return regex1[0].length === regex2[0].length;
+    return regex1.length === regex2.length;
   }
   return false;
 };
@@ -16,7 +13,13 @@ const sameAmount = (str, exp1, exp2) => {
 // console.log(sameAmount('hellow', /h/, /w/));○
 // console.log(sameAmount('hello', /h/, /w/));
 // console.log(sameAmount('q', /q /, /qqqqqqq/));
-console.log(sameAmount('qqqq', /qqqq /, /qqqqqqq/));
+console.log(
+  sameAmount(
+    'qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd',
+    /qqqq /,
+    /qqqqqqq/
+  )
+);
 // console.log(sameAmount('hello how are you', /l/, /e/));
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/global
