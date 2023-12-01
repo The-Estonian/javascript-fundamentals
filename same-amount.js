7af430d7cf36cd5958e204b2e42b56aca2d99751
The-Estonian <Zaar2213@gmail.com>
2023-12-01 11:58:34 +0200

    trying different approach on same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,14 @@
 const sameAmount = (str, exp1, exp2) => {
-  try {
-    return str.match(exp1).length == str.match(exp2).length;
-  } catch {
-    return false;
+  let regex1 = str.match(exp1);
+  let regex2 = str.match(exp2);
+  if (regex1 !== null && regex2 !== null) {
+    regex1.length == regex2.length;
   }
+  return false;
 };
 
-// console.log(sameAmount('hellow', /h/, /w/));
+// console.log(sameAmount('hellow', /h/, /w/));○
 // console.log(sameAmount('hello', /h/, /w/));
+// console.log(sameAmount('q', /q /, /qqqqqqq/));
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/global
