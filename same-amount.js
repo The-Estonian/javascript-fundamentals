7f2365125b4870a9f6fd1113450485747dd3401f
The-Estonian <Zaar2213@gmail.com>
2023-12-01 12:11:09 +0200

    forgot to return the comparision

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,11 @@
 const sameAmount = (str, exp1, exp2) => {
   let regex1 = str.match(exp1);
   let regex2 = str.match(exp2);
+  //   console.log(regex1[0].length);
+  //   console.log(regex2[0].length);
   if (regex1 !== null && regex2 !== null) {
-    regex1.length == regex2.length;
+    console.log('MATCHING');
+    return regex1[0].length === regex2[0].length;
   }
   return false;
 };
@@ -10,5 +13,6 @@ const sameAmount = (str, exp1, exp2) => {
 // console.log(sameAmount('hellow', /h/, /w/));○
 // console.log(sameAmount('hello', /h/, /w/));
 // console.log(sameAmount('q', /q /, /qqqqqqq/));
+// console.log(sameAmount('hello how are you', /l/, /e/));
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/global
