6caf6f8cff9c388a83d2eb95047c1ac90f76df3f
The-Estonian <Zaar2213@gmail.com>
2023-12-01 16:29:10 +0200

    started greedy url

---
+++ greedy-url.js
@@ -0,0 +1,45 @@
+const getURL = (url) => {
+  var cleanURL = url.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  );
+  return cleanURL;
+};
+
+const greedyQuery = (url) => {
+  var allURLs = getURL(url);
+  //   console.log(allURLs);
+  const newURLArr = [];
+  for (let i = 0; i < allURLs.length; i++) {
+    const matches = allURLs[i].match(/[?&]([^=]+)=([^&]+)/g);
+    // if (matches !== null && matches.length > 1) {
+    //     console.log(matches.length);
+    // }
+    if (matches !== null && matches.length > 1) {
+      newURLArr.push(allURLs[i]);
+    }
+  }
+  return newURLArr;
+};
+
+const notSoGreedy = (url) => {
+  var allURLs = getURL(url);
+  //   console.log(allURLs);
+  const newURLArr = [];
+  for (let i = 0; i < allURLs.length; i++) {
+    const matches = allURLs[i].match(/[?&]([^=]+)=([^&]+)/g);
+    // if (matches !== null && matches.length > 1) {
+    //     console.log(matches.length);
+    // }
+    if (matches !== null && matches.length >= 2 && matches.length < 4) {
+      newURLArr.push(allURLs[i]);
+    }
+  }
+  return newURLArr;
+};
+
+testurl =
+  'qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you';
+
+// console.log(getURL(testurl));
+// console.log(greedyQuery(testurl));
+// console.log(notSoGreedy(testurl));
