da958523eddf85d8a8283aa324f390b1dae59d76
The-Estonian <Zaar2213@gmail.com>
2023-12-01 10:45:06 +0200

    second try on same amount

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,10 @@
 const sameAmount = (str, exp1, exp2) => {
-  return str.match(exp1).length == str.match(exp2).length;
+  try {
+    return str.match(exp1).length == str.match(exp2).length;
+  } catch {
+    return false;
+  }
 };
+
+// console.log(sameAmount('hellow', /h/, /w/));
+// console.log(sameAmount('hello', /h/, /w/));
