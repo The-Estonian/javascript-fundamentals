8b90d0c6b487bc4f2033e1359d2983e5484bb739
The-Estonian <Zaar2213@gmail.com>
2023-12-01 15:16:22 +0200

    removed commas that caused index errors

--- ion-out.js
+++ ion-out.js
@@ -2,12 +2,14 @@ const ionOut = (str) => {
   const returnArray = [];
   let regex = /tion/g;
   const inputArray = str.split(' ');
+//   console.log(inputArray[0].slice(0, -4));
   for (let i = 0; i < inputArray.length; i++) {
     if (inputArray[i].match(regex)) {
     //   console.log('REGEX OK');
-      returnArray.push(inputArray[i].slice(0, -3));
+      returnArray.push(inputArray[i].replace(/,/g, '').slice(0, -3));
     }
   }
   return returnArray;
 };
 // console.log(ionOut('This ption is pretty dtion right?'));
+// console.log(ionOut('attention, direction'));
