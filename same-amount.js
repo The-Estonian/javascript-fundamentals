aa12b6dc88f1291af8e42b8f275c78bfe0b1d7af
The-Estonian <Zaar2213@gmail.com>
2023-12-01 12:14:13 +0200

    fetching input information

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,13 @@
 const sameAmount = (str, exp1, exp2) => {
+  let testCounter = 0;
+  console.log(str);
   let regex1 = str.match(exp1);
   let regex2 = str.match(exp2);
   //   console.log(regex1[0].length);
   //   console.log(regex2[0].length);
   if (regex1 !== null && regex2 !== null) {
-    console.log('MATCHING');
+    console.log(testCounter);
+    testCounter++;
     return regex1[0].length === regex2[0].length;
   }
   return false;
@@ -13,6 +16,7 @@ const sameAmount = (str, exp1, exp2) => {
 // console.log(sameAmount('hellow', /h/, /w/));○
 // console.log(sameAmount('hello', /h/, /w/));
 // console.log(sameAmount('q', /q /, /qqqqqqq/));
+console.log(sameAmount('qqqq', /qqqq /, /qqqqqqq/));
 // console.log(sameAmount('hello how are you', /l/, /e/));
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/global
