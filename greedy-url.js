9dbc42bf0ec76ec92533ca1d62bddd6216432a01
The-Estonian <Zaar2213@gmail.com>
2023-12-01 16:56:07 +0200

    changed another regex

--- greedy-url.js
+++ greedy-url.js
@@ -9,7 +9,9 @@ const greedyQuery = (url) => {
   var allURLs = getURL(url);
   const newURLArr = [];
   for (let i = 0; i < allURLs.length; i++) {
-    const matches = allURLs[i].match(/[?&]([^=]+)=([^&]+)/g);
+    const matches = allURLs[i].match(
+      /\?([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*&){2,255}([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*)/g
+    );
     if (matches !== null && matches.length > 1) {
       newURLArr.push(allURLs[i]);
     }
@@ -32,7 +34,10 @@ const notSoGreedy = (url) => {
 // testurl =
 //   'qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you';
 // testurl2 = 'http://www_example.com/';
+var test3 =
+  'http://hummm/how?how=come&same=[123,21]&you=nextperson&id=123312&next=123DSAD&ok=true&notOk=true';
 // console.log(getURL(testurl));
 // console.log(greedyQuery(testurl));
 // console.log(notSoGreedy(testurl));
 // console.log(getURL(testurl2));
+console.log(greedyQuery(test3));
