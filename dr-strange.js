1b1dd67ec6c2b6e544a6b24a8ecf39722ee55040
The-Estonian <Zaar2213@gmail.com>
2023-12-04 11:33:39 +0200

    second try on strange

--- dr-strange.js
+++ dr-strange.js
@@ -16,11 +16,13 @@ const addWeek = (obj) => {
     'secondSunday',
   ];
 
-  var inputTime = new Date(obj).getDate();
-  while (inputTime > 14) {
-    inputTime -= 14;
+  currentDate = new Date(obj);
+  startDate = new Date(currentDate.getFullYear(), 0, 1);
+  var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+  if (Math.ceil(days / 7) % 2 == 0) {
+    return dayNames[currentDate.getDay() + 6];
   }
-  return dayNames[inputTime - 1];
+  return dayNames[currentDate.getDay() - 1];
 };
 
 const timeTravel = (obj) => {
@@ -40,8 +42,9 @@ const timeTravel = (obj) => {
 
 //Fri May 29 2020 21:22:22 GMT+0100 (Western European Summer Time)
 
-console.log(addWeek(new Date('0001-01-01')));
-console.log(addWeek(new Date('0001-01-02')));
-console.log(addWeek(new Date('0001-01-07')));
-console.log(addWeek(new Date('0001-01-08')));
-console.log(addWeek(new Date('0001-01-09')));
+// console.log(addWeek(new Date('0001-01-01')));
+// console.log(addWeek(new Date('0001-01-02')));
+// console.log(addWeek(new Date('0001-01-07')));
+// console.log(addWeek(new Date('0001-01-08')));
+// console.log(addWeek(new Date('0001-01-09')));
+// console.log(addWeek(new Date('2025-08-11')));
