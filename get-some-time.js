4d859c552ec1024dabbbd655b184e91a7e8535fd
The-Estonian <Zaar2213@gmail.com>
2023-12-04 14:14:26 +0200

    first requirement done

--- get-some-time.js
+++ get-some-time.js
@@ -1,4 +1,7 @@
 const firstDayWeek = (week, year) => {
   console.log(week);
   console.log(year);
+  return '01-' + (week < 10 ? '0' + String(week) : String(week)) + '-' + year;
 };
+
+// console.log(firstDayWeek(1, '1000')); // '01-01-1000'
