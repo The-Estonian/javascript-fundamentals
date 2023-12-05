417008ae2fbf864ca571153412ee4abeaba8cd07
The-Estonian <Zaar2213@gmail.com>
2023-12-05 16:34:07 +0200

    started long words

---
+++ long-words.js
@@ -0,0 +1,15 @@
+const longWords = (data) => {
+  return data.every((item) => item.length >= 5);
+};
+
+const oneLongWord = (data) => {
+  return data.some((item) => {
+    return item.length >= 10;
+  });
+};
+
+const noLongWords = (data) => {
+  return data.some((item) => {
+    return !(item.length >= 7);
+  });
+};
