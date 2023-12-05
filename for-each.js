ed61dd42e5cfbfd515fdc1568407746d72ad466a
The-Estonian <Zaar2213@gmail.com>
2023-12-05 19:50:52 +0200

    wrote tests, let's see.

--- for-each.js
+++ for-each.js
@@ -4,3 +4,12 @@ const forEach = (arr, func) => {
   }
   return undefined;
 };
+
+// let arr = [1, 2, 3, 4, 5, Math.random(), 7, 10, -10, 20, -95];
+// let result = [];
+// let move = (item) => {
+//   result.push(item);
+// };
+
+// let one = forEach(arr, move);
+// console.log([one, result]);
