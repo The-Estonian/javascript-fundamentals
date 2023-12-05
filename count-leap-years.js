0141565c98f624efc0d691f4852fb534660a45e3
The-Estonian <Zaar2213@gmail.com>
2023-12-05 10:36:21 +0200

    testing leaper again

--- count-leap-years.js
+++ count-leap-years.js
@@ -4,9 +4,10 @@ function leaper(year) {
 
 const countLeapYears = (date) => {
   let counter = 0;
-  for (let i = 0; i < date.getFullYear(); i++) {
+  for (let i = 1; i <= date.getFullYear(); i++) {
     counter += leaper(i) ? 1 : 0;
   }
   return counter;
 };
 
+// console.log(countLeapYears(new Date('0001-12-01')));
