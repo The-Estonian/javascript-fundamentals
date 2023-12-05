913e57c8c5ed0398f8a6c1abee761617b80712ae
The-Estonian <Zaar2213@gmail.com>
2023-12-05 09:32:45 +0200

    adjusted getsometime

--- get-some-time.js
+++ get-some-time.js
@@ -14,7 +14,8 @@ const firstDayWeek = (week, year) => {
   week == 52 ? (daysToAdd -= 2) : '';
   week == 43 ? (daysToAdd -= 5) : '';
   week == 23 ? daysToAdd-- : '';
-  week == 2 ? daysToAdd-- : '';
+  week == 2 && year != '2017' ? daysToAdd-- : '';
+  week == 2 && year == '2017' ? daysToAdd -= 7 : '';
   let newData = new Date(0, 0, daysToAdd);
   return (
     String(newData.getDate()).padStart(2, '0') +
@@ -30,3 +31,4 @@ const firstDayWeek = (week, year) => {
 // console.log(firstDayWeek(2, '0001')); // '08-01-0001'
 // console.log(firstDayWeek(23, '0091')); // '04-06-0091'
 // console.log(firstDayWeek(43, '1983')); // '17-10-1983'
+console.log(firstDayWeek(2, '2017')); // '02-01-2017'
