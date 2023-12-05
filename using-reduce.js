609d8782fe7ac186f32767f435337e7240759a66
The-Estonian <Zaar2213@gmail.com>
2023-12-05 15:29:39 +0200

    testing adder

--- using-reduce.js
+++ using-reduce.js
@@ -1,10 +1,14 @@
-const adder = (data) => {
-  return data.reduce((start, end) => {
-    return start + end;
-  });
+const adder = (data, leftover = 0) => {
+  return (
+    leftover +
+    data.reduce((start, end) => {
+      return start + end;
+    })
+  );
 };
 
 // console.log(adder([1, 2, 3, 4])); // 10
+console.log(adder([9, 24, 7, 11, 3], 10)); // 64
 
 const sumOrMul = (data) => {
   return data.reduce((hold, run) => {
@@ -23,4 +27,3 @@ const funcExec = (data) => {
     return run(hold);
   });
 };
-
