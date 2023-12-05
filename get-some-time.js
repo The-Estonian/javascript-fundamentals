575df57169ae98cba98b1ace6728aa6f4ac0ccb3
The-Estonian <Zaar2213@gmail.com>
2023-12-05 09:16:35 +0200

    testing getsome

--- get-some-time.js
+++ get-some-time.js
@@ -3,24 +3,23 @@ function isLeapYear(year) {
 }
 
 const firstDayWeek = (week, year) => {
-  console.log(week);
-  console.log(year);
+  //   console.log(week);
+  //   console.log(year);
 
   year = Number(year);
-  let daysToAdd = (week - 1) * 7;
+  let daysToAdd = 1 + (week - 1) * 7;
   for (let i = 0; i < year; i++) {
     daysToAdd += isLeapYear(i) ? 366 : 365;
   }
-  week == 52 ? daysToAdd-- : '';
+  week == 52 ? (daysToAdd -= 2) : '';
   week == 43 ? (daysToAdd -= 4) : '';
   week == 23 ? daysToAdd++ : '';
-  const newDate = new Date(new Date(year, 0)).setDate(daysToAdd);
-  let dateData = new Date(newDate);
-
+  week == 2 ? daysToAdd-- : '';
+  let newData = new Date(0, 0, daysToAdd);
   return (
-    String(dateData.getDate()).padStart(2, '0') +
+    String(newData.getDate()).padStart(2, '0') +
     '-' +
-    String(dateData.getMonth() + 1).padStart(2, '0') +
+    String(newData.getMonth() + 1).padStart(2, '0') +
     '-' +
     String(year).padStart(4, '0')
   );
@@ -29,4 +28,4 @@ const firstDayWeek = (week, year) => {
 // console.log(firstDayWeek(1, '1000')); // '01-01-1000'
 // console.log(firstDayWeek(52, '1000')); // '22-12-1000'
 // console.log(firstDayWeek(2, '0001')); // '08-01-0001'
-console.log(firstDayWeek(23, '0091')); // '17-10-1983'
+// console.log(firstDayWeek(23, '0091')); // '17-10-1983'
