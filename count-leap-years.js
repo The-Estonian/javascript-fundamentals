61e9e583bae80820ddecf324281713e47af6d178
The-Estonian <Zaar2213@gmail.com>
2023-12-05 10:38:03 +0200

    adjused index mistake

--- count-leap-years.js
+++ count-leap-years.js
@@ -4,10 +4,11 @@ function leaper(year) {
 
 const countLeapYears = (date) => {
   let counter = 0;
-  for (let i = 1; i <= date.getFullYear(); i++) {
+  for (let i = 1; i < date.getFullYear(); i++) {
     counter += leaper(i) ? 1 : 0;
   }
   return counter;
 };
 
-// console.log(countLeapYears(new Date('0001-12-01')));
+console.log(countLeapYears(new Date('0001-12-01'))); // 0
+console.log(countLeapYears(new Date('1664-08-09'))); // 403
