4f27e8afdc5f912e92aa8823dbd58e71c50354f8
The-Estonian <Zaar2213@gmail.com>
2023-12-05 16:17:23 +0200

    trying sumorMul

--- using-reduce.js
+++ using-reduce.js
@@ -4,9 +4,14 @@ const adder = (data, leftover = 0) => {
 
 // console.log(adder([1, 2, 3, 4])); // 10
 // console.log(adder([9, 24, 7, 11, 3], 10)); // 64
-console.log(adder([]), 0); // 0
+// console.log(adder([]), 0); // 0
 
-const sumOrMul = (data) => {
+const sumOrMul = (data, leftover = 0) => {
+  if (data[0] % 2 == 0) {
+    data[0] *= leftover;
+  } else {
+    data[0] += leftover;
+  }
   return data.reduce((hold, run) => {
     if (run % 2 == 0) {
       return hold * run;
@@ -17,6 +22,7 @@ const sumOrMul = (data) => {
 };
 
 // console.log(sumOrMul([29, 23, 3, 2, 25])); // 135
+console.log(sumOrMul([18, 17, 7, 13, 25], 12)); // 278
 
 const funcExec = (data) => {
   return data.reduce((hold, run) => {
