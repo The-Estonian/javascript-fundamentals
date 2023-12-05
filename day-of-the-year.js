fad0b754b23a495ba944e7e8e0b0f2bf9bde2851
The-Estonian <Zaar2213@gmail.com>
2023-12-05 11:08:38 +0200

    testing get day of year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,5 +1,12 @@
 const dayOfTheYear = (date) => {
-  return (date.getTime() + 62135596800000) / 1000 / 60 / 60 / 24 + 1;
+  let months = date.getMonth();
+  let counter = 0;
+  for (let i = 0; i < months; i++) {
+    counter += new Date(date.getFullYear(), i, 0).getDate();
+    // console.log(counter);
+  }
+  return counter + date.getDate();
 };
 
 // console.log(dayOfTheYear(new Date('0005-01-01')));
+console.log(dayOfTheYear(new Date('1664-08-09'))); // 222
