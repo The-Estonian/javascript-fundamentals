49744feefa003753fb9a602964c7269db7503199
The-Estonian <Zaar2213@gmail.com>
2023-12-05 11:13:07 +0200

    testing day of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,12 +1,17 @@
-const dayOfTheYear = (date) => {
-  let months = date.getMonth();
-  let counter = 0;
-  for (let i = 0; i < months; i++) {
-    counter += new Date(date.getFullYear(), i, 0).getDate();
-    // console.log(counter);
-  }
-  return counter + date.getDate();
-};
+function leaper(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
 
-// console.log(dayOfTheYear(new Date('0005-01-01')));
+const dayOfTheYear = (date) => {
+  const startOfYear = new Date(2000, 0, 1);
+
+  date.setFullYear(2000);
+  const timeD = date - startOfYear;
+
+  const days = Math.floor(timeD / (24 * 60 * 60 * 1000)) + 1;
+
+  return days;
+};
+console.log(dayOfTheYear(new Date('0005-01-01'))); // 1
 console.log(dayOfTheYear(new Date('1664-08-09'))); // 222
+console.log(dayOfTheYear(new Date('1600-12-31'))); // 366
