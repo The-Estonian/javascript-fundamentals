929e4e086743c9b3b92534b16877f49737dbeff2
The-Estonian <Zaar2213@gmail.com>
2023-12-06 12:47:06 +0200

    started quantifiers

---
+++ quantifiers.js
@@ -0,0 +1,28 @@
+const every = (arr, func) => {
+  let trigger = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (!func(arr[i])) {
+      return false;
+    }
+  }
+  return trigger;
+};
+
+const some = (arr, func) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (!func(arr[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const none = (arr, func) => {
+  let trigger = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (!func(arr[i])) {
+      return false;
+    }
+  }
+  return trigger;
+};
