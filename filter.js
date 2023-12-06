a5fbc0d092ea2a1bc5d7f6f1ef1f3e7c71cb77b7
The-Estonian <Zaar2213@gmail.com>
2023-12-06 11:35:57 +0200

    created partition

--- filter.js
+++ filter.js
@@ -18,6 +18,19 @@ const reject = (arr, func) => {
   return newArr;
 };
 
+const partition = () => {
+  let winners = [];
+  let outcast = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i], i, arr)) {
+      winners.push(arr[i]);
+    } else {
+      outcast.push(arr[i]);
+    }
+  }
+  return [winners, outcast];
+};
+
 // const check1 = (el) => el % 2 === 0;
 
 // console.log(filter([10, -10, 20, 86, 3, 32, 450, 950, 66, 150], check1));
