10471eb70caa771295440641d0a296cbd7341134
The-Estonian <Zaar2213@gmail.com>
2023-12-06 11:53:45 +0200

    fixed foldRight

--- reduce.js
+++ reduce.js
@@ -8,11 +8,16 @@ const fold = (arr, func, acc) => {
 
 const foldRight = (arr, func, acc) => {
   let baseValue = acc;
-  for (let i = arr.length; i >= 0; i--) {
+  for (let i = arr.length-1; i >= 0; i--) {
     baseValue = func(baseValue, arr[i]);
   }
   return baseValue;
 };
 
+// const reduce  = (arr, func, acc=arr[0]){
+//     if (arr == )
+// }
+
 // const adder = (a, b) => a + b;
 // console.log(fold([1, 2, 3], adder, 2));
+// console.log(foldRight([3, 10, 26, 0], adder, 0));
