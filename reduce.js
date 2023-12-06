83a960f1037a8a516e4aab58610f5e9408baf8bc
The-Estonian <Zaar2213@gmail.com>
2023-12-06 12:07:32 +0200

    fixed reduceRight

--- reduce.js
+++ reduce.js
@@ -30,7 +30,7 @@ const reduceRight = (arr, func) => {
     throw new Error('Arr < 0');
   }
   let baseValue = arr[0];
-  for (let i = arr.length - 1; i >= 0; i--) {
+  for (let i = arr.length - 1; i > 0; i--) {
     baseValue = func(baseValue, arr[i]);
   }
   return baseValue;
@@ -40,3 +40,4 @@ const reduceRight = (arr, func) => {
 // console.log(fold([1, 2, 3], adder, 2));
 // console.log(foldRight([3, 10, 26, 0], adder, 0));
 // console.log(reduce([3, 10, 26, 0], adder, 0));
+// console.log(reduceRight([3, 10, 26, 0], adder, 0));
