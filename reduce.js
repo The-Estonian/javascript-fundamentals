fe3dab55eda5021cd5e42d5a152de0b7410b2acc
The-Estonian <Zaar2213@gmail.com>
2023-12-06 12:18:30 +0200

    adjusted reduceRight

--- reduce.js
+++ reduce.js
@@ -29,15 +29,19 @@ const reduceRight = (arr, func) => {
   if (arr.length === 0) {
     throw new Error('Arr < 0');
   }
-  let baseValue = arr[0];
-  for (let i = 1; i < arr.length; i++) {
+  let baseValue = arr[arr.length - 1];
+  for (let i = arr.length - 2; i >= 0; i--) {
     baseValue = func(baseValue, arr[i]);
   }
   return baseValue;
 };
 
 // const adder = (a, b) => a + b;
+// const concatenate = (a = '', b) => a.concat(b);
 // console.log(fold([1, 2, 3], adder, 2));
 // console.log(foldRight([3, 10, 26, 0], adder, 0));
 // console.log(reduce([3, 10, 26, 0], adder, 0));
-// console.log(reduceRight(["examplesimple", "a", "is", "This"], adder, 0));
+// console.log(reduceRight([3, 10, 26, 0], adder, 0));
+// console.log(
+//   reduceRight(['This ', 'is ', 'a ', 'simple ', 'example'], concatenate, 0)
+// );
