5b5e378ee432b08871fb97f7b4592423fdf19626
The-Estonian <Zaar2213@gmail.com>
2023-12-06 12:58:50 +0200

    started sweet curry

---
+++ sweet-curry.js
@@ -0,0 +1,23 @@
+const add = (n) => {
+  return (n1) => {
+    return n * n1;
+  };
+};
+
+const add3 = (n) => {
+  return (n1) => {
+    return (n2) => {
+      return n + n1 + n2;
+    };
+  };
+};
+
+const sub4 = (n) => {
+  return (n1) => {
+    return (n2) => {
+      return (n3) => {
+        return n - n1 - n2 - n3;
+      };
+    };
+  };
+};
