c83fe1457e2a25676c045881b3d5d8106c22b95d
The-Estonian <Zaar2213@gmail.com>
2023-12-06 11:57:53 +0200

    started reduce

--- reduce.js
+++ reduce.js
@@ -8,15 +8,22 @@ const fold = (arr, func, acc) => {
 
 const foldRight = (arr, func, acc) => {
   let baseValue = acc;
-  for (let i = arr.length-1; i >= 0; i--) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     baseValue = func(baseValue, arr[i]);
   }
   return baseValue;
 };
 
-// const reduce  = (arr, func, acc=arr[0]){
-//     if (arr == )
-// }
+const reduce = (arr, func, acc) => {
+  if (arr.length === 0) {
+    throw new Error('Arr < 0');
+  }
+  let baseValue = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    baseValue = func(baseValue, arr[i]);
+  }
+  return baseValue;
+};
 
 // const adder = (a, b) => a + b;
 // console.log(fold([1, 2, 3], adder, 2));
