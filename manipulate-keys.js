c619ea5d31b3fb94b7bff68c73e24d799c2f643f
The-Estonian <Zaar2213@gmail.com>
2023-12-11 12:45:34 +0200

    testing mani again

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -16,7 +16,10 @@ const mapKeys = (obj, func) => {
   return returnObj;
 };
 
-const reduceKeys = (obj, func, i = func(obj)[1]) => {
+const reduceKeys = (obj, func, i="") => {
+  if (Array.isArray(obj)) {
+    return obj.reduce(func, i);
+  }
   let returnObj = [];
   for (const [key, value] of Object.entries(obj)) {
     returnObj.push(key);
@@ -24,7 +27,7 @@ const reduceKeys = (obj, func, i = func(obj)[1]) => {
   return returnObj.reduce(func, i);
 };
 
-// const nutrients = ['vinegar, sugar, oil, onion, garlic, paprika'];
+const nutrients = ['vinegar, sugar, oil, onion, garlic, paprika'];
 
 // // console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
 // // // output: { protein: 20 }
@@ -32,8 +35,8 @@ const reduceKeys = (obj, func, i = func(obj)[1]) => {
 // // console.log(mapKeys(nutrients, (k) => `-${k}`))
 // // // output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
 
-// console.log(reduceKeys(nutrients, (acc, cr) =>acc.concat(', ', cr)))
-// // output: carbohydrates, protein, fat
+console.log(reduceKeys(nutrients, (acc, cr) => acc.concat(', ', cr)));
+// output: carbohydrates, protein, fat
 
 // // // eq(
 // //     reduceKeys(ctx.cart, (acc, cr) => `${acc}${cr}:`, ':'),
@@ -45,3 +48,8 @@ const reduceKeys = (obj, func, i = func(obj)[1]) => {
 
 // // + 'vinegarsugar:oil:onion:garlic:paprika:'
 // // - ':vinegar:sugar:oil:onion:garlic:paprika:'
+
+// if (typeof res !== "number") {
+//     if (res.slice(0, 2) === ", ") res = res.slice(2);
+//     if (undef && res[0] === ":") res = res.slice(1);
+// }
