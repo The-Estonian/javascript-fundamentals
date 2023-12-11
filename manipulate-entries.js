fc88f8832888e3e3b1ab6cc00169dfb07d581467
The-Estonian <Zaar2213@gmail.com>
2023-12-11 15:00:19 +0200

    fixed comments out

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -39,7 +39,6 @@ const lowCarbs = (objEntry) => {
   for (const [key, value] of Object.entries(objEntry)) {
     if (nutritionDB.hasOwnProperty(key)) {
       if ((nutritionDB[key].carbs * value) / 100 > 50) {
-        // console.log('IS ABOVE: ', key, (nutritionDB[key].carbs * value) / 100);
         delete objEntry[key];
       }
     }
@@ -64,58 +63,58 @@ const cartTotal = (objEntry) => {
 //Object.keys(obj);
 //Object.values(obj);
 
-const nutritionDB = {
-  tomato: {
-    calories: 18,
-    protein: 0.9,
-    carbs: 3.9,
-    sugar: 2.6,
-    fiber: 1.2,
-    fat: 0.2,
-  },
-  vinegar: {
-    calories: 20,
-    protein: 0.04,
-    carbs: 0.6,
-    sugar: 0.4,
-    fiber: 0,
-    fat: 0,
-  },
-  oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
-  onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
-  garlic: {
-    calories: 149,
-    protein: 6.4,
-    carbs: 33,
-    sugar: 1,
-    fiber: 2.1,
-    fat: 0.5,
-  },
-  paprika: {
-    calories: 282,
-    protein: 14.14,
-    carbs: 53.99,
-    sugar: 1,
-    fiber: 0,
-    fat: 12.89,
-  },
-  sugar: {
-    calories: 387,
-    protein: 0,
-    carbs: 100,
-    sugar: 100,
-    fiber: 0,
-    fat: 0,
-  },
-  orange: {
-    calories: 49,
-    protein: 0.9,
-    carbs: 13,
-    sugar: 9,
-    fiber: 0.2,
-    fat: 0.1,
-  },
-};
+// const nutritionDB = {
+//   tomato: {
+//     calories: 18,
+//     protein: 0.9,
+//     carbs: 3.9,
+//     sugar: 2.6,
+//     fiber: 1.2,
+//     fat: 0.2,
+//   },
+//   vinegar: {
+//     calories: 20,
+//     protein: 0.04,
+//     carbs: 0.6,
+//     sugar: 0.4,
+//     fiber: 0,
+//     fat: 0,
+//   },
+//   oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
+//   onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
+//   garlic: {
+//     calories: 149,
+//     protein: 6.4,
+//     carbs: 33,
+//     sugar: 1,
+//     fiber: 2.1,
+//     fat: 0.5,
+//   },
+//   paprika: {
+//     calories: 282,
+//     protein: 14.14,
+//     carbs: 53.99,
+//     sugar: 1,
+//     fiber: 0,
+//     fat: 12.89,
+//   },
+//   sugar: {
+//     calories: 387,
+//     protein: 0,
+//     carbs: 100,
+//     sugar: 100,
+//     fiber: 0,
+//     fat: 0,
+//   },
+//   orange: {
+//     calories: 49,
+//     protein: 0.9,
+//     carbs: 13,
+//     sugar: 9,
+//     fiber: 0.2,
+//     fat: 0.1,
+//   },
+// };
 
 // const groceriesCart = { orange: 500, oil: 20, sugar: 480 };
 // const groceriesCart1 = { oil: 500, onion: 230, garlic: 220, paprika: 480 };
