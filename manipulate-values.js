f14d4d5b3b68c1932da0a327481f604d876d0201
The-Estonian <Zaar2213@gmail.com>
2023-12-11 12:04:17 +0200

    started manipulate values

---
+++ manipulate-values.js
@@ -0,0 +1,40 @@
+const filterValues = (obj, func) => {
+  let returnObj = {};
+  for (const [key, value] of Object.entries(obj)) {
+    if (func(value)) {
+      returnObj[key] = value;
+    }
+  }
+  return returnObj;
+};
+
+const mapValues = (obj, func) => {
+  let returnObj = {};
+  for (const [key, value] of Object.entries(obj)) {
+    returnObj[key] = func(value);
+  }
+  return returnObj;
+};
+
+const reduceValues = (obj, func) => {
+  let returnObj = [];
+  for (const [key, value] of Object.entries(obj)) {
+    returnObj.push(value);
+  }
+  return returnObj.reduce(func);
+};
+
+//Object.entries(obj);
+//Object.keys(obj);
+//Object.values(obj);
+
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
+
+console.log(filterValues(nutrients, (nutrient) => nutrient <= 12));
+// output: { carbohydrates: 12, fat: 5 }
+
+console.log(mapValues(nutrients, (v) => v + 1));
+// output: { carbohydrates: 13, protein: 21, fat: 6 }
+
+console.log(reduceValues(nutrients, (acc, cr) => acc + cr));
+// output: 37
