3c20c143a61da8ffc1f8ce5169d23aeee6386aef
The-Estonian <Zaar2213@gmail.com>
2023-12-11 12:07:28 +0200

    testing manipulate values

--- manipulate-values.js
+++ manipulate-values.js
@@ -16,25 +16,27 @@ const mapValues = (obj, func) => {
   return returnObj;
 };
 
-const reduceValues = (obj, func) => {
+const reduceValues = (obj, func, i) => {
   let returnObj = [];
   for (const [key, value] of Object.entries(obj)) {
     returnObj.push(value);
   }
-  return returnObj.reduce(func);
+  return returnObj.reduce(func, i);
 };
 
 //Object.entries(obj);
 //Object.keys(obj);
 //Object.values(obj);
 
-const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
+// const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
 
-console.log(filterValues(nutrients, (nutrient) => nutrient <= 12));
-// output: { carbohydrates: 12, fat: 5 }
+// console.log(filterValues(nutrients, (nutrient) => nutrient <= 12));
+// // output: { carbohydrates: 12, fat: 5 }
 
-console.log(mapValues(nutrients, (v) => v + 1));
-// output: { carbohydrates: 13, protein: 21, fat: 6 }
+// console.log(mapValues(nutrients, (v) => v + 1));
+// // output: { carbohydrates: 13, protein: 21, fat: 6 }
 
-console.log(reduceValues(nutrients, (acc, cr) => acc + cr));
-// output: 37
+// console.log(reduceValues(nutrients, (acc, cr) => acc + cr));
+// // output: 37
+
+// console.log(reduceValues({ a: 1, b: 2, c: 3 }, (acc, cr) => acc + cr, 3));
