e6382219b3e3e08f8b6a44f84becc657b5fd0528
The-Estonian <Zaar2213@gmail.com>
2023-12-12 15:32:32 +0200

    adjusted replica again

--- replica.js
+++ replica.js
@@ -2,9 +2,9 @@ const replica = (target, ...sources) => {
   if (sources.length < 2) {
     for (const [key, value] of Object.entries(sources[0])) {
       if (target.hasOwnProperty(key)) {
-        if (Array.isArray(value)) {
+        if (Array.isArray(value) || Array.isArray(target[key])) {
           target[key] = value;
-        } else if (typeof value === 'object') {
+        } else if (typeof value !== 'object') {
           target[key] = value;
         } else {
           target[key] = Object.assign(target[key], value);
@@ -44,3 +44,4 @@ const replica = (target, ...sources) => {
 // console.log(replica({ a: 4 }, { a: { b: 1 } }).a.b); // 1
 // console.log(replica({ a: 2 }, { a: [4] })); // [4]
 // console.log(Object.assign({ a: [1, 2, 4] }, { a: { b: [4] } }).a);
+// console.log(Object.assign(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }))); //{ a: { b: 1, c: 23 } }
