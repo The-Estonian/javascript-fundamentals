40a063bdff9c5285e3928b353485a2dec35f05e0
The-Estonian <Zaar2213@gmail.com>
2023-12-12 11:43:34 +0200

    adjusted deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,4 +1,11 @@
 const deepCopy = (obj) => {
+  if (Array.isArray(obj)) {
+    let newArray = [];
+    for (let i = 0; i < obj.length; i++) {
+      newArray.push(obj[i]);
+    }
+    return newArray;
+  }
   let target = Array.isArray(obj) ? [] : {};
   for (let prop in obj) {
     let value = obj[prop];
@@ -9,5 +16,13 @@ const deepCopy = (obj) => {
     }
   }
   return target;
-}
+};
 
+// const copyAndCompare = (obj) => {
+//   const copy = deepCopy(Object.freeze(obj));
+//   console.log(copy);
+//   console.log(obj);
+//   return copy !== obj;
+// };
+
+// console.log(copyAndCompare([console.log, /hello/]));
