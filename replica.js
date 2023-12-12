5a12d3c67c125bdce1d722d2d0f2ce7cc1e50ab1
The-Estonian <Zaar2213@gmail.com>
2023-12-12 12:49:19 +0200

    adjusted replica

--- replica.js
+++ replica.js
@@ -1,7 +1,25 @@
-const replica = (target, ...sources) => {
-  return Object.assign(target, ...sources);
+const isObject = (item) => {
+  return item && typeof item === 'object' && !Array.isArray(item);
 };
 
+const replica = (target, ...sources) => {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        replica(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return replica(target, ...sources);
+};
+// Object.prototype.hasOwnProperty();
 // console.log(replica({ con: console.log }, { reg: /hello/ }));
 
 // {
@@ -18,3 +36,7 @@ const replica = (target, ...sources) => {
 // );
 
 // { line: 'Replicants are like any other machine', author: 'Rich' }
+
+// console.log(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }));
+
+// { a: { b: 1, c: 23 } }
