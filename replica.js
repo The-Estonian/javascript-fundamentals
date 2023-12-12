9083db28744331f31c19e54a8f9bbffe0c2ae41b
The-Estonian <Zaar2213@gmail.com>
2023-12-12 14:48:50 +0200

    testing replica

--- replica.js
+++ replica.js
@@ -1,30 +1,17 @@
-const isObject = (item) => {
-  return item && typeof item === 'object' && !Array.isArray(item);
+const replica = (target, ...sources) => {
+  sources.forEach((e) => {
+    for (const [key, value] of Object.entries(e)) {
+      if (target.hasOwnProperty(key)) {
+        target[key] = Object.assign(target[key], value);
+      } else {
+        target[key] = value;
+      }
+    }
+  });
+  return target;
 };
 
-// const replica = (target, ...sources) => {
-//   console.log([...sources]);
-//   if ([...sources][0] == /hello/) {
-//     console.log('MATCH');
-//     return Object.assign(target, ...sources);
-//   }
-//   if (!sources.length) return target;
-//   const source = sources.shift();
-
-//   if (isObject(target) && isObject(source)) {
-//     for (const key in source) {
-//       if (isObject(source[key])) {
-//         if (!target[key]) Object.assign(target, { [key]: {} });
-//         replica(target[key], source[key]);
-//       } else {
-//         Object.assign(target, { [key]: source[key] });
-//       }
-//     }
-//   }
-
-//   return replica(target, ...sources);
-// };
-
+//------------------------------------------------------------------------
 // Object.prototype.hasOwnProperty();
 
 // {
@@ -42,43 +29,44 @@ const isObject = (item) => {
 
 // { line: 'Replicants are like any other machine', author: 'Rich' }
 
-// console.log(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }));
+// console.log(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } })); // { a: { b: 1, c: 23 } }
 
-// { a: { b: 1, c: 23 } }
-function replica(target, ...sources) {
-  if (sources.length > 1) {
-    let tempObj = {};
-    for (let i = 0; i < sources.length; i++) {
-      for (const [key, value] of Object.entries(sources[i])) {
-        tempObj[key] = value;
-      }
-    }
-    return tempObj;
-  }
-  if (!sources.length) return target;
-  const source = sources.shift();
+// function replica(target, ...sources) {
+//   if (sources.length > 1) {
+//     let tempObj = {};
+//     for (let i = 0; i < sources.length; i++) {
+//       for (const [key, value] of Object.entries(sources[i])) {
+//         tempObj[key] = value;
+//       }
+//     }
+//     return tempObj;
+//   }
+//   if (!sources.length) return target;
+//   const source = sources.shift();
 
-  if (
-    typeof source !== 'object' ||
-    source === null ||
-    source instanceof RegExp ||
-    typeof source === 'function'
-  ) {
-    return source;
-  }
+//   if (
+//     typeof source !== 'object' ||
+//     source === null ||
+//     source instanceof RegExp ||
+//     typeof source === 'function'
+//   ) {
+//     return source;
+//   }
 
-  if (source instanceof Array) {
-    return source.reduce((arr, item, i) => {
-      arr[i] = replica(null, item); // Use null as target for arrays
-      return arr;
-    }, []);
-  } else if (source instanceof Object) {
-    return Object.keys(source).reduce((newObj, key) => {
-      newObj[key] = replica(newObj[key], source[key]); // Use existing property as target for objects
-      return newObj;
-    }, target || {}); // Use target if provided, otherwise create a new object
-  }
-}
+//   if (source instanceof Array) {
+//     return source.reduce((arr, item, i) => {
+//       arr[i] = replica(null, item); // Use null as target for arrays
+//       return arr;
+//     }, []);
+//   } else if (source instanceof Object) {
+//     return Object.keys(source).reduce((newObj, key) => {
+//       newObj[key] = replica(newObj[key], source[key]); // Use existing property as target for objects
+//       return newObj;
+//     }, target || {}); // Use target if provided, otherwise create a new object
+//   }
+// }
+
+// ------------------------------------------------------------------------
 
 // console.log(replica({ con: console.log }, { reg: /hello/ }));
 // console.log(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }));
@@ -89,3 +77,5 @@ function replica(target, ...sources) {
 //     Object.freeze({ author: 'Rich' })
 //   )
 // );
+
+// console.log(replica({ a: 4 }, { a: { b: 1 } }).a.b); // 1
