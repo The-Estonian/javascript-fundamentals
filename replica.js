f2e165d200ad53ad85ee5ba047f9a200358a970a
The-Estonian <Zaar2213@gmail.com>
2023-12-12 12:26:08 +0200

    adjusted replica

--- replica.js
+++ replica.js
@@ -1,10 +1,20 @@
-const replica = (target, sources) => {
-  return Object.assign(target, sources);
+const replica = (target, ...sources) => {
+  return Object.assign(target, ...sources);
 };
 
-console.log(replica({ con: console.log }, { reg: /hello/ }));
+// console.log(replica({ con: console.log }, { reg: /hello/ }));
 
 // {
 //   con: console.log,
 //   reg: /hello/,
 // }
+
+// console.log(
+//   replica(
+//     {},
+//     Object.freeze({ line: 'Replicants are like any other machine' }),
+//     Object.freeze({ author: 'Rich' })
+//   )
+// );
+
+// { line: 'Replicants are like any other machine', author: 'Rich' }
