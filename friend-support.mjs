b8a7063eabca6196602f4fe78404bfe6db9c9039
The-Estonian <Zaar2213@mail.com>
2023-12-16 23:15:38 +0200

    forgot and commented out the server start message

--- friend-support.mjs
+++ friend-support.mjs
@@ -27,29 +27,6 @@ let server = http.createServer((request, response) => {
     'Content-Length': Buffer.byteLength(data),
   });
   response.end();
-
-  // try {
-  //   const dataJSON = fs
-  //     .readFileSync('./guests' + req.url + '.json')
-  //     .toString('utf8');
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.statusCode = 200;
-  //   res.write(dataJSON);
-  //   res.end();
-  //   //   }
-  //   // }
-  // } catch (err) {
-  //   // console.log(err);
-  //   if (err.code == 'ENOENT') {
-  //     res.setHeader('Content-Type', 'application/json');
-  //     res.statusCode = 404;
-  //     res.end(Buffer.from(JSON.stringify({ error: 'guest not found' })));
-  //   } else {
-  //     res.setHeader('Content-Type', 'application/json');
-  //     res.statusCode = 500;
-  //     res.end(Buffer.from(JSON.stringify({ error: 'server failed' })));
-  //   }
-  // }
 });
-// console.log('Server on http://localhost:5000');
+console.log('Server on http://localhost:5000');
 server.listen(5000);
