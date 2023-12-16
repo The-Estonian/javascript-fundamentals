23d88a12a6cede34c4ab56c05ae255650fee1be2
The-Estonian <Zaar2213@mail.com>
2023-12-16 22:42:56 +0200

    refactored whole friend support

--- friend-support.mjs
+++ friend-support.mjs
@@ -1,32 +1,55 @@
 import * as http from 'node:http';
 import * as fs from 'fs';
 
-let server = http.createServer(function (req, res) {
-  try {
-    // const allFileNames = fs.readdirSync('./guests');
-    // for (let i = 0; i < allFileNames.length; i++) {
-    //   if (req.url == '/' + allFileNames[i].replace('.json', '')) {
-    const dataJSON = fs
-      .readFileSync('./guests' + req.url + '.json')
-      .toString('utf8');
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
-    res.write(dataJSON);
-    res.end();
-    //   }
-    // }
-  } catch (err) {
-    // console.log(err);
-    if (err.code == 'ENOENT') {
-      res.setHeader('Content-Type', 'application/json');
-      res.statusCode = 404;
-      res.end(Buffer.from(JSON.stringify({ error: 'guest not found' })));
-    } else {
-      res.setHeader('Content-Type', 'application/json');
-      res.statusCode = 500;
-      res.end(Buffer.from(JSON.stringify({ error: 'server failed' })));
+let server = http.createServer((request, response) => {
+  const { method, url, headers } = request;
+  let data;
+  let statusC;
+  if (method === 'GET' && request.url !== '/favicon.ico') {
+    try {
+      let fileData = fs.readFileSync('./guestss' + request.url + '.json');
+      statusC = 200;
+      data = fileData.toString('utf8');
+    } catch (err) {
+      // console.log(err);
+      if (err.code == 'ENOENT') {
+        statusC = 404;
+        data = JSON.stringify({ error: 'guest not found' });
+      } else {
+        statusC = 500;
+        data = JSON.stringify({ error: 'server failed' });
+      }
     }
   }
+  // response.statusCode = statusC;
+  response.writeHead(statusC, {
+    'Content-Length': Buffer.byteLength(data),
+    'Content-Type': 'application/json',
+  });
+  response.end();
+
+  // try {
+  //   const dataJSON = fs
+  //     .readFileSync('./guests' + req.url + '.json')
+  //     .toString('utf8');
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.statusCode = 200;
+  //   res.write(dataJSON);
+  //   res.end();
+  //   //   }
+  //   // }
+  // } catch (err) {
+  //   // console.log(err);
+  //   if (err.code == 'ENOENT') {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     res.statusCode = 404;
+  //     res.end(Buffer.from(JSON.stringify({ error: 'guest not found' })));
+  //   } else {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     res.statusCode = 500;
+  //     res.end(Buffer.from(JSON.stringify({ error: 'server failed' })));
+  //   }
+  // }
 });
 console.log('Server on http://localhost:5000');
 server.listen(5000);
