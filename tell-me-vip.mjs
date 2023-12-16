0fb69c05a6d6dd7c347c808c319ffac3f4eb5f1a
The-Estonian <Zaar2213@mail.com>
2023-12-16 10:03:43 +0200

    adjusted vip

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -3,37 +3,30 @@ import { readdir } from 'node:fs/promises';
 import * as fs from 'fs';
 // const { readFileSync } = require('fs');
 
-// const files = [
-//   ['Ubaid_Ballard.json', { answer: 'no' }],
-//   ['Victoria_Chan.json', { answer: 'no' }],
-//   ['Dominika_Mullen.json', { answer: 'no' }],
-//   ['Heath_Denton.json', { answer: 'no' }],
-//   ['Lilith_Hamilton.json', { answer: 'no' }],
-// ];
-
 let nameArray = [];
 let enteredDir = process.argv[2];
-try {
-  // const files = await readdir(enteredDir);
-  for (let i = 0; i < files.length; i++) {
-    let [firstname, lastname] = files[i].replace('.json', '').split('_');
-    // console.log(`${i + 1}. ${lastname} ${firstname}`)
-    let formattedName = lastname + ' ' + firstname;
 
-    const dataJSON = fs.readFileSync('./guests/' + files[i]);
-    let vipStatus = JSON.parse(dataJSON);
-    if (vipStatus.answer === 'yes') {
-      nameArray.push(formattedName);
-    }
-    //   console.log(vipStatus.answer);
+const files = await readdir(enteredDir, (err, files) => {
+  if (err) {
+    return [''];
   }
-  nameArray.sort();
-  console.log(nameArray);
-  for (let i = 0; i < nameArray.length; i++) {
-    nameArray[i] = i + 1 + '. ' + nameArray[i];
+});
+for (let i = 0; i < files.length; i++) {
+  let [firstname, lastname] = files[i].replace('.json', '').split('_');
+  // console.log(`${i + 1}. ${lastname} ${firstname}`)
+  let formattedName = lastname + ' ' + firstname;
+
+  const dataJSON = fs.readFileSync('./guests/' + files[i]);
+  let vipStatus = JSON.parse(dataJSON);
+  if (vipStatus.answer === 'yes') {
+    nameArray.push(formattedName);
   }
-} catch (err) {
-  nameArray = [''];
+  //   console.log(vipStatus.answer);
+}
+nameArray.sort();
+console.log(nameArray);
+for (let i = 0; i < nameArray.length; i++) {
+  nameArray[i] = i + 1 + '. ' + nameArray[i];
 }
 console.log(nameArray);
 
@@ -42,6 +35,14 @@ fs.writeFile('./vip.txt', nameArray.join('\n'), { flag: 'w+' }, (err) => {
     console.log(err);
   }
 });
+
+// const files = [
+//   ['Ubaid_Ballard.json', { answer: 'no' }],
+//   ['Victoria_Chan.json', { answer: 'no' }],
+//   ['Dominika_Mullen.json', { answer: 'no' }],
+//   ['Heath_Denton.json', { answer: 'no' }],
+//   ['Lilith_Hamilton.json', { answer: 'no' }],
+// ];
 
 // const { readFileSync } = require('fs');
 // const data = readFileSync('./config.json');
