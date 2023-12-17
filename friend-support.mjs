19fb13ca30b8e30906ee83da45325741027546b3
The-Estonian <Zaar2213@mail.com>
2023-12-17 22:48:29 +0200

    testing error code again

--- friend-support.mjs
+++ friend-support.mjs
@@ -6,51 +6,51 @@ let server = http.createServer((request, response) => {
   const { method, url, headers } = request;
   response.setHeader('Content-Type', 'application/json');
   if (method === 'GET' && request.url !== '/favicon.ico') {
-    try {
-      // server.kill();
-      fs.readFile('./guests' + request.url + '.json', 'utf8', (err, data) => {
-        try {
+    // try {
+    // server.kill();
+    fs.readFile('./guests' + request.url + '.json', 'utf8', (err, data) => {
+      try {
+        response
+          .writeHead(200, {
+            'Content-Length': Buffer.byteLength(data),
+          })
+          .end(data);
+      } catch (err) {
+        console.log(err.code);
+        if (err.code !== 'ERR_INVALID_ARG_TYPE') {
+          let data = JSON.stringify({ error: 'guest not found' });
           response
-            .writeHead(200, {
+            .writeHead(404, {
               'Content-Length': Buffer.byteLength(data),
             })
             .end(data);
-        } catch (err) {
-          console.log(err.code);
-          if (err.code === 'ERR_INVALID_ARG_TYPE') {
-            let data = JSON.stringify({ error: 'guest not found' });
-            response
-              .writeHead(404, {
-                'Content-Length': Buffer.byteLength(data),
-              })
-              .end(data);
-          } else {
-            let data = JSON.stringify({ error: 'server failed' });
-            response
-              .writeHead(500, {
-                'Content-Length': Buffer.byteLength(data),
-              })
-              .end(data);
-          }
+        } else {
+          let data = JSON.stringify({ error: 'server failed' });
+          response
+            .writeHead(500, {
+              'Content-Length': Buffer.byteLength(data),
+            })
+            .end(data);
         }
-      });
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        let data = JSON.stringify({ error: 'guest not found' });
-        response
-          .writeHead(404, {
-            'Content-Length': Buffer.byteLength(data),
-          })
-          .end(data);
-      } else {
-        let data = JSON.stringify({ error: 'server failed' });
-        response
-          .writeHead(500, {
-            'Content-Length': Buffer.byteLength(data),
-          })
-          .end(data);
       }
-    }
+    });
+    // } catch (err) {
+    //   if (err.code === 'ENOENT') {
+    //     let data = JSON.stringify({ error: 'guest not found' });
+    //     response
+    //       .writeHead(404, {
+    //         'Content-Length': Buffer.byteLength(data),
+    //       })
+    //       .end(data);
+    //   } else {
+    //     let data = JSON.stringify({ error: 'server failed' });
+    //     response
+    //       .writeHead(500, {
+    //         'Content-Length': Buffer.byteLength(data),
+    //       })
+    //       .end(data);
+    //   }
+    // }
   }
   const asd = response.getHeaders();
   console.log(asd);
