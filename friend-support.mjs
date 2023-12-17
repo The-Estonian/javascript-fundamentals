62e3eee29304fa32bf7dbef4cd0de8a7fe5c19fa
The-Estonian <Zaar2213@mail.com>
2023-12-17 20:53:47 +0200

    testing new version of friend support

--- friend-support.mjs
+++ friend-support.mjs
@@ -3,28 +3,31 @@ import * as fs from 'fs';
 
 let server = http.createServer((request, response) => {
   const { method, url, headers } = request;
-  let data = '';
-  let statusC = 200;
   if (method === 'GET' && request.url !== '/favicon.ico') {
+    response.setHeader('Content-Type', 'application/json');
     try {
-      let fileData = fs.readFileSync('./guests' + request.url + '.json');
-      statusC = 200;
-      data = fileData.toString('utf8');
+      fs.readFile('./guests' + request.url + '.json', 'utf8', (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        response.statusCode = 200;
+        try {
+          response.write(data);
+        } catch (err) {
+          response.statusCode = 404;
+          response.write(
+            Buffer.from(JSON.stringify({ error: 'guest not found' }))
+          );
+          response.end();
+        }
+        response.end();
+      });
     } catch (err) {
-      // console.log(err);
-      if (err.code == 'ENOENT') {
-        statusC = 404;
-        data = JSON.stringify({ error: 'guest not found' });
-      } else {
-        statusC = 500;
-        data = JSON.stringify({ error: 'server failed' });
-      }
+      response.statusCode = 500;
+      response.write(JSON.stringify({ error: 'server failed' }), 'utf8');
+      response.end();
     }
   }
-  response.setHeader('Content-Type', 'application/json');
-  response.statusCode = statusC;
-  response.write(data, 'utf8');
-  response.end();
 });
 console.log('Server on http://localhost:5000');
 server.listen(5000);
