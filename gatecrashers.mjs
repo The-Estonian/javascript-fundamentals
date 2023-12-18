c03a83472db760fe2480b02dacb15de98f677944
The-Estonian <Zaar2213@gmail.com>
2023-12-18 15:11:02 +0200

    testing gate crashers

---
+++ gatecrashers.mjs
@@ -0,0 +1,129 @@
+import * as http from 'node:http';
+import * as fs from 'fs';
+import { Buffer } from 'node:buffer';
+
+const AUTHORIZED_USERS = {
+  Caleb_Squires: 'abracadabra',
+  Tyrique_Dalton: 'abracadabra',
+  Rahima_Young: 'abracadabra',
+};
+
+const server = http.createServer((request, response) => {
+  const { method, url, headers } = request;
+  response.setHeader('Content-Type', 'application/json');
+  console.log(method);
+  if (method === 'POST' && url !== '/favicon.ico') {
+    // Security part starts here
+    console.log(request.headers);
+    const authheader = request.headers.authorization;
+    if (!authheader) {
+      response.writeHead(401, {
+        'Content-Length': 0,
+        'WWW-Authenticate': 'Basic realm="Authorization Required"',
+      });
+      response.end();
+    }
+
+    const auth = Buffer.from(authheader.split(' ')[1], 'base64')
+      .toString()
+      .split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    // console.log(auth[0])
+    // console.log(AUTHORIZED_USERS[user])
+
+    // console.log(auth[1])
+    // console.log(pass)
+
+    if (AUTHORIZED_USERS[user] === pass) {
+      // Security part ends here
+      const body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on('end', () => {
+        // kui viitsimist siis võib proovida ilma asyncita
+        const data = Buffer.concat(body).toString();
+        const filePath = `./guests${url}.json`;
+
+        try {
+          console.log('trying');
+          fs.promises.writeFile(filePath, data, {
+            encoding: 'utf8',
+            flag: 'w',
+          }); // ta muidu sulgeb selle faili ja ei jõua lõpuni kirjutada
+          const successResponse = JSON.stringify(
+            JSON.parse(data.toString('utf8'))
+          );
+
+          console.log(successResponse);
+          response.writeHead(201, {
+            'Content-Length': Buffer.byteLength(successResponse),
+          });
+          response.end(successResponse);
+        } catch (error) {
+          const serverFailedResponse = JSON.stringify({
+            error: 'server failed in catch',
+          });
+          response.writeHead(500, {
+            'Content-Length': Buffer.byteLength(serverFailedResponse),
+          });
+          response.end(serverFailedResponse);
+        }
+      });
+    } else {
+      // New else security error
+      response.writeHead(401, {
+        'Content-Length': 0,
+        'WWW-Authenticate': 'Basic realm="Authorization Required"',
+      });
+      response.end();
+      // End new else security error
+    }
+  } else if (method === 'POST' && url !== '/favicon.ico') {
+    try {
+      const body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on('end', async () => {
+        // kui viitsimist siis võib proovida ilma asyncita
+        const data = Buffer.concat(body).toString();
+        const filePath = `./guests${url}.json`;
+
+        try {
+          await fs.promises.writeFile(filePath, data, {
+            encoding: 'utf8',
+            flag: 'w',
+          }); // ta muidu sulgeb selle faili ja ei jõua lõpuni kirjutada
+          response.writeHead(201, {
+            'Content-Length': Buffer.byteLength(data),
+          });
+          response.end(data);
+        } catch (error) {
+          const serverFailedResponse = JSON.stringify({
+            error: 'server failed',
+          });
+          response.writeHead(500, {
+            'Content-Length': Buffer.byteLength(serverFailedResponse),
+          });
+          response.end(serverFailedResponse);
+        }
+      });
+    } catch (error) {
+      const serverFailedResponse = JSON.stringify({ error: 'server failed' });
+      response.writeHead(500, {
+        'Content-Length': Buffer.byteLength(serverFailedResponse),
+      });
+      response.end(serverFailedResponse);
+    }
+  }
+});
+
+server.listen(5000, () => {
+  console.log('Server on http://localhost:5000');
+});
+
+// https://stackoverflow.com/questions/4295782/how-to-process-post-data-in-node-js
